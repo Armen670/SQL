91bcb34be5527f3ebefcf94abefa030ed434d975
Armen670 <armen.baburian@yandex.ru>
2023-12-01 15:28:24 +0300

    Solve some execr

---
+++ 2/032 ДЗ _группировка, сопоставление, сортировка_/Решение.sql
@@ -0,0 +1,30 @@
+/* 1 */
+SELECT *
+FROM orders
+WHERE ship_country LIKE 'U%'
+/* 2 */
+SELECT order_id, customer_id, freight, ship_country
+FROM orders
+WHERE ship_country LIKE 'N%'
+ORDER BY freight DESC
+LIMIT 10
+/* 3 */
+SELECT first_name, last_name, home_phone, region
+FROM employees
+WHERE region IS NULL
+/* 4 */
+SELECT COUNT(*)
+FROM customers
+WHERE region IS NOT NULL
+/* 5 */
+SELECT COUNT(*),country
+FROM suppliers
+GROUP BY country
+ORDER BY COUNT(*) DESC
+/* 6 */
+SELECT SUM(freight), ship_country
+FROM orders
+WHERE ship_region IS NOT NULL
+GROUP BY ship_country
+HAVING SUM(freight) > 2750
+ORDER BY SUM(freight) DESC
